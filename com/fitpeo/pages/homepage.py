920e8fbd3622c365462dbec0b07adc6139867b35
RishithaNaga <nagarishitha181292@gmail.com>
2024-11-27 18:29:41 +0530

    committing the enhancements made by defining the elements xpath as variables

--- com/fitpeo/pages/homepage.py
+++ com/fitpeo/pages/homepage.py
@@ -2,14 +2,16 @@ from selenium.webdriver import ActionChains, Keys
 from selenium.webdriver.common.by import By
 
 class HomePage:
+    button_menu_xpath='//button[@aria-label="open drawer"]'
+    button_revenuecalculator_xpath='//span[text()="Revenue Calculator"]'
     def __init__(self, driver):
         self.driver = driver
         self.url = "https://www.fitpeo.com/"
 
 
     def navigate_to_revenue_calculator(self):
-        self.driver.find_element(By.XPATH, '//button[@aria-label="open drawer"]').click()
-        self.driver.find_element(By.XPATH, '//span[text()="Revenue Calculator"]').click()
+        self.driver.find_element(By.XPATH,HomePage.button_menu_xpath ).click()
+        self.driver.find_element(By.XPATH,HomePage.button_revenuecalculator_xpath ).click()
 
     def navig(self):
         self.driver.get(self.url)
--- com/fitpeo/pages/revenuecalculatorpage.py
+++ com/fitpeo/pages/revenuecalculatorpage.py
@@ -5,22 +5,28 @@ from com.fitpeo.exceptions.valuemismatchexception import ValueMismatchException
 
 
 class RevenueCalculatorPage:
+
+    employee_slidertextfield_xpath='//input[@aria-invalid="false"]'
+    employee_slider_xpath='//span[contains(@class, "MuiSlider-thumb")]'
+    employee_slider_textfield_attributevalue='//span[contains(@class, "MuiSlider-thumb")]/child::input'
+    text_total_recurring_reimbursement_xpath="//*[text()='Total Recurring Reimbursement for all Patients Per Month']/following-sibling::p"
+
     def __init__(self, driver):
         self.driver = driver
 
     def type_in_slider_text_field(self, number):
-        slider_text_field_element = self.driver.find_element(By.XPATH, '//input[@aria-invalid="false"]')
+        slider_text_field_element = self.driver.find_element(By.XPATH,RevenueCalculatorPage.employee_slidertextfield_xpath )
         slider_text_field_element.click()
         for _ in range(4):
             slider_text_field_element.send_keys(Keys.BACKSPACE)
         slider_text_field_element.send_keys(number)
 
     def move_slider_to(self, number):
-        slider_element = self.driver.find_element(By.XPATH, '//span[contains(@class, "MuiSlider-thumb")]')
+        slider_element = self.driver.find_element(By.XPATH,RevenueCalculatorPage.employee_slider_xpath )
         action = ActionChains(self.driver)
         action.click_and_hold(slider_element).perform()
 
-        current_value_element = self.driver.find_element(By.XPATH, '//span[contains(@class, "MuiSlider-thumb")]/child::input')
+        current_value_element = self.driver.find_element(By.XPATH,RevenueCalculatorPage.employee_slider_textfield_attributevalue )
         current_value = int(current_value_element.get_attribute('value'))
 
         key_to_press = Keys.RIGHT
@@ -34,7 +40,7 @@ class RevenueCalculatorPage:
 
     def verify_slider_percentage(self, number):
         expected_percentage = (number * 100) / 2000
-        slider_element = self.driver.find_element(By.XPATH, '//span[contains(@class, "MuiSlider-thumb")]')
+        slider_element = self.driver.find_element(By.XPATH, RevenueCalculatorPage.employee_slider_xpath)
         style_attr_value = slider_element.get_attribute('style')
         actual_percentage = float(style_attr_value.replace("left: ", "").replace("%;", "").strip())
 
@@ -58,7 +64,7 @@ class RevenueCalculatorPage:
             checkbox_element.click()
 
     def verify_total_recurring_reimbursement_per_month(self, expected):
-        actual = self.driver.find_element(By.XPATH, "//*[text()='Total Recurring Reimbursement for all Patients Per Month']/following-sibling::p").text
+        actual = self.driver.find_element(By.XPATH,RevenueCalculatorPage.text_total_recurring_reimbursement_xpath ).text
         if actual != expected:
             raise ValueMismatchException(
                 f'Total Recurring Reimbursement Per Month is incorrect. Actual = {actual}, Expected = {expected}'
